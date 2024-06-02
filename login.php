2190239b7a30d77cdc7076621240985f94077ae0
NATASHA BINTI KHAIRULNIZAM <ntsh.nzm20@gmail.com>
2024-06-02 23:37:30 +0800

    initial commit

--- login.php
+++ login.php
@@ -30,16 +30,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login Page</title>
-</head>
-<body>
-  <h2>Login</h2>
-  <?php include 'login.php'; ?>
-</body>
-</html>
